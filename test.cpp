b44cc2013e29590bce8939a5f468b341240ccf58
foodycookie <146843264+foodycookie@users.noreply.github.com>
2023-11-02 18:58:20 +0000

    im coming

--- test.cpp
+++ test.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+class inventory {
+    public:
+        int id;
+
+        inventory(int id): id(id) {}
+};
+
+class category: public inventory {
+    public:
+        string type;
+
+        category(string type,int id): inventory(id),type(type) {}
+};
+
+class keyboard: public category {
+    public:
+        string name;
+        string brand;
+        double price;
+
+        keyboard(string name,string brand,double price,string type,int id): category(type,id),name(name),brand(brand),price(price) {}
+};
+
 int main() {
-    cout << "and that is precisely why i do bedroom cardio with leon edwards.";
-    cout << "Hello";
+    keyboard keyboard1("MXMECHANICAL","Logitech",979,"keyboard",1);
+
+    cout << keyboard1.id << endl << keyboard1.type << endl << keyboard1.name << endl << keyboard1.brand << endl << keyboard1.price;
 }
